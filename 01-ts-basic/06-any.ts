b17e38c23bf2329aa87395b32ce9137688e9f007
dahye1013 <alrean@naver.com>
2021-12-23 00:16:43 +0900

    [01-ts-basic] add sample code - enum, any, union

---
+++ 01-ts-basic/06-any.ts
@@ -0,0 +1,16 @@
+/**
+ * [Any]
+ * - flexible type
+ * - no specific type assignment
+ *
+ * -> big disadvantage 😥
+ *
+ * only use when really can't know which kind of data will be sotred in there!!!
+ *
+ *
+ */
+
+{
+  let favoriteActivities: any[];
+  favoriteActivities = ["sport"];
+}
